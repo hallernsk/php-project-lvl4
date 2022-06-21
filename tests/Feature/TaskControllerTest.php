2760bfe2d68421c1cc01e51e83080995e95c3158
hallernsk <haller@ngs.ru>
2022-06-22 00:52:32 +0700

    fixed DatabaseSeeder

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Feature;
 
 use App\Models\Task;
-use App\Models\User;
 use App\Models\TaskStatus;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,33 +16,38 @@ class TaskControllerTest extends TestCase
      *
      * @return void
      */
+ //   use RefreshDatabase;
+
     private User $user;
 
     protected function setUp(): void
     {
         parent::setUp();
-     //   $task = Task::factory()->make();
-        //    dd($task);
-     //   $taskStatus = TaskStatus::factory()->make();
-        //    dd($taskStatus);
+     //   Task::factory()->count(2)->make();
+    //    $task = Task::factory()->count(2)->make();
+    //    dd($task);
+    //    TaskStatus::factory()->count(2)->make();
+    //    $taskStatus = TaskStatus::factory()->count(2)->make();
+    //    dd($taskStatus);
+
         $this->user = User::factory()->create();
-        //       dd($this->user);
+ //       dd($this->user);
         TaskStatus::factory()->create();
     }
 
     public function testIndex()
     {
         $response = $this->get(route('tasks.index'));
-  //      dd($response);
         $response->assertOk();
     }
 
     public function testShow()
     {
         $task = Task::factory()->create();
-  //      dd($task);
-        $response = $this->get(route('tasks.index', $task));
-  //      dd($response);
+        //     dd($task);
+        //    $taskStatus = TaskStatus::factory()->count(2)->make();
+        $response = $this->get(route('tasks.show', $task));
+        //   dd($response);
         $response->assertOk();
     }
 
@@ -55,8 +60,9 @@ class TaskControllerTest extends TestCase
     public function testStore()
     {
         $data = Task::factory()->make()->only('name', 'description', 'status_id', 'assigned_to_id');
-        //   dd($data);
+     //   dd($data);
         $response = $this->actingAs($this->user)->post(route('tasks.store'), $data);
+    //    dd($response);
         $response->assertRedirect(route('tasks.index'));
         $response->assertSessionHasNoErrors();
         $this->assertDatabaseHas('tasks', $data);
@@ -65,11 +71,12 @@ class TaskControllerTest extends TestCase
     public function testEdit()
     {
         $task = Task::factory()->create();
-//        dd($task);
+        //    dd($task);
+        //    $taskStatus = TaskStatus::factory()->count(2)->make();
         $response = $this->get(route('tasks.edit', $task));
+        //   dd($response);
         $response->assertOk();
     }
-
 
     public function testUpdate()
     {
@@ -89,7 +96,7 @@ class TaskControllerTest extends TestCase
     {
         $task = Task::factory()->create();
         //      dd($task);
-        $response = $this->delete(route('tasks.destroy', [$task]));
+        $response = $this->delete(route('tasks.destroy', $task));
         //     dd($response);
         $response->assertSessionHasNoErrors();
         $response->assertRedirect(route('tasks.index'));
