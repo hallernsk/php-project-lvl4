d3ee8bd2802f3e2a22a0a4525049114d2d3689f4
hallernsk <hallaer@ngs.ru>
2022-07-27 23:54:07 +0700

    forced https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        Url::forceScheme('https');
     }
 }
