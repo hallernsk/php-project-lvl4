f85b286981d200cbeba8cdeb2104dd8ad9d5dc51
hallernsk <hallaer@ngs.ru>
2022-08-01 20:35:30 +0700

    fixed web.php(group routes)

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::resource('task_statuses', TaskStatusController::class);
-
-Route::resource('tasks', TaskController::class);
-
-Route::resource('labels', LabelController::class);
+Route::resources([
+    'task_statuses' => TaskStatusController::class,
+    'tasks' => TaskController::class,
+    'labels' => LabelController::class,
+]);
