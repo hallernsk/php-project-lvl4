0bab98dddb4329d6215dac1bfcae59bbef49b4c0
hallernsk <hallaer@ngs.ru>
2023-12-15 13:29:45 +0700

    fixed routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskStatusController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\LabelController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
